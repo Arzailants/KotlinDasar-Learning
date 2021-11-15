686e211aef0a196322d5b8eaea53635e37d53908
arzailants <andrerizaldib@gmail.com>
2021-11-15 11:09:49 +0700

    Membuat Variable

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    // VARIABLE DI KOTLIN, TERDAPAT 2
+    // var = membuat variable yang mutable = bisa dirubah
+    // val = membuat variable yang immutable = tidak bisa dirubah
+
+    // MUTABLE / VAR = BISA DIRUBAH
+    var data1 = "Andre Rizaldi Brillianto"
+    data1 = "Alice"
+
+    println(data1)
+
+    // IMMUTABLE / VAL = TIDAK BISA DIRUBAH
+    val dataImmutable = "Andre Rizaldi Brillianto"
+//    dataImmutable = "AliceMyLove"
+
+    println(dataImmutable)
+}
