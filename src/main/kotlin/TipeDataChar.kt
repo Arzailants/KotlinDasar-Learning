421f8dbaf71350aa1033b5af013e97eb6130d508
arzailants <andrerizaldib@gmail.com>
2021-11-15 10:34:23 +0700

    Menambahkan Tipe data Char

---
+++ src/main/kotlin/TipeDataChar.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    var tipeChar: Char = 'A';
+    var tipeChar2: Char = 'N';
+    var tipeChar3: Char = 'D';
+    var tipeChar4: Char = 'R';
+    var tipeChar5: Char = 'E';
+
+    println(tipeChar);
+    println(tipeChar2);
+    println(tipeChar3);
+    println(tipeChar4);
+    println(tipeChar5);
+}
