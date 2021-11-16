6ece1912c641ad7e420838c1eae006b8100916f6
arzailants <andrerizaldib@gmail.com>
2021-11-16 20:16:09 +0700

    Menambahkan Nullable dan Variable Constant

---
+++ src/main/kotlin/Nullable.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    // DI KOTLIN, KITA BISA MEMAKSA VARIABLE MENJADI NULL
+    // DENGAN MENAMBAHKAN ? DI BELAKANG TIPE DATANYA
+    var data:String? = "Andre Rizaldi Brillianto"
+    data = null
+
+    println(data)
+
+
+//    =============================================================
+
+    var data2:String? = null
+    data2 = "Andre Rizaldi Brillianto"
+
+    println(data2)
+    println(data2.length)
+}
---
+++ src/main/kotlin/VariableConstant.kt
@@ -0,0 +1,9 @@
+// MEMBUAT VARIABLE CONSTANTS
+// CONSTANT ADALAH VARIABLE GLOBAL YANG DAPAT DIAKSES DARIMANAPUN
+const val APPLICATION ="Panda Mobile";
+const val VERSION = 1.0
+
+fun main(){
+    println("Nama Aplikasi = " +APPLICATION)
+    println("Versi Aplikasi = " +VERSION)
+}
