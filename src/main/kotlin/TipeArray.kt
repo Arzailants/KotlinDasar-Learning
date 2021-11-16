ece7657d1d69c80fbab51f07dd53d53bbaa061f8
arzailants <andrerizaldib@gmail.com>
2021-11-16 20:39:37 +0700

    Membuat Class Tipe Array di Kotlin

---
+++ src/main/kotlin/TipeArray.kt
@@ -0,0 +1,7 @@
+fun main(){
+
+    // MEMBUAT ARRAY, DENGAN KOTLIN GENERIC
+    val arrayString: Array<String> = arrayOf("Andre", "Rizaldi", "Brillianto")
+    val arrayInteger: Array<Int> = arrayOf(1,2,3,4,5,6,7,8)
+    val arrayChar: Array<Char> = arrayOf('A','B','C','D','E')
+}
