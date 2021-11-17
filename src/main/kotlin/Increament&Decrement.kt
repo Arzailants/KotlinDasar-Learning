af0cfbeb029e08984ad7a1952ab2608ac50533f6
arzailants <andrerizaldib@gmail.com>
2021-11-17 15:33:54 +0700

    Menambahkan Increment_Decrement dan Operasi Matematika

---
+++ src/main/kotlin/Increament&Decrement.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    var total = 10
+    // INCREMENT
+    total += 10
+
+    println(total)
+
+    total++
+
+    println(total)
+
+
+    // DECREMENT
+    total -= 5
+
+    println(total)
+
+    total--
+
+    println(total)
+}
