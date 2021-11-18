8599d8f542ffaee11beaacf18a0ceb6cf39a7907
arzailants <andrerizaldib@gmail.com>
2021-11-18 12:03:10 +0700

    Menambahkan When Expression

--- src/main/kotlin/WhenExpression.kt
+++ src/main/kotlin/WhenExpression.kt
@@ -1,7 +1,7 @@
 fun main(){
 
     // MEMBUAT WHEN EXPRESSION / SWITCH EXPRESSION DI BAHASA PEMROGRAMAN JAVA
-    val nilaiUjian = 'R'
+    var nilaiUjian = 'D'
 
     when(nilaiUjian){
         'A' -> println("Selamat Anda Mendapatkan Nilai = " +nilaiUjian)
@@ -10,5 +10,45 @@ fun main(){
         'D' -> println("Selamat Anda Mendapatkan Nilai = " +nilaiUjian)
         'E' -> println("Selamat Anda Mendapatkan Nilai = " +nilaiUjian)
         else -> println("Maaf Anda Harus Mengulang")
+    }
+
+
+    // WHEN EXPRESSION MULTIPLE OPINION
+    when (nilaiUjian){
+        'A', 'B', 'C' -> {
+            println("Selamat Nilai Anda Sempurna")
+        }
+        else -> {
+            println("Maaf Anda Gagal")
+        }
+    }
+
+
+
+    // WHEN EXPRESSION IN
+    val nilaiLulus: Array<Char> = arrayOf('A','B','C')
+    when(nilaiUjian){
+        in nilaiLulus -> println("Selamat Anda Lulus Sempurna")
+        !in nilaiLulus -> println("Maaf Anda Goblok")
+    }
+
+
+
+    // WHEN EXPRESSION IS
+    val data = 'A'
+    when(data){
+        is Char -> println("Ini adalah Tipe Data Char")
+        !is Char -> println("Ini Bukan Tipe Data Char")
+    }
+
+
+
+    // WHEN TANDA VARIABLE
+    val dataWhen = 'A'
+    when {
+        dataWhen == 'A' -> println("Selamat anda Mendapatkan Nilai A")
+        dataWhen == 'B' -> println("Selamat anda Mendapatkan Nilai B")
+        dataWhen == 'C' -> println("Selamat anda Mendapatkan Nilai C")
+        else -> println("Maaf Salah")
     }
 }
