330761d4e950a610eb8fcc112d53290806e62d7f
arzailants <andrerizaldib@gmail.com>
2021-11-18 11:17:11 +0700

    Menambahkan Operasi Boolean

---
+++ src/main/kotlin/OperasiBoolean.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+    // MEMBUAT OPERASI BOOLEAN
+    // &&
+    // |
+    // !=
+
+    // && = DAN
+    val nilaiMatematika = 80
+    val nilaiFisika = 69
+
+    val lulusMatematika = nilaiMatematika > 75
+    val lulusFisika = nilaiFisika > 75
+
+    val lulusDAN = lulusMatematika && lulusFisika
+    println(lulusDAN)
+
+    // | = ATAU
+    val lulusATAU = lulusMatematika || lulusFisika
+    println(lulusATAU)
+}
