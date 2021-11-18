98709d8c41a39abbfa5e01dd386e665670c126d2
arzailants <andrerizaldib@gmail.com>
2021-11-18 11:39:13 +0700

    Menambahkan If Expression dan Whe Expression

---
+++ src/main/kotlin/IfExpression.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    // MEMBUAT IF EXPRESSION
+    val data = 80
+    val data2 = 65
+
+    if (data >= 75){
+        println("Selamat anda lulus")
+    }
+
+
+    // MEMBUAT IF ELSE EXPRESSION
+    if (data2 > 85){
+        println("Nilai anda = " + data2+ ", Selamat Anda Lulus")
+    } else {
+        println("Nilai anda = " +data2+ ", Maaf Anda gagal")
+    }
+}
---
+++ src/main/kotlin/WhenExpression.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    // MEMBUAT WHEN EXPRESSION / SWITCH EXPRESSION DI BAHASA PEMROGRAMAN JAVA
+    val nilaiUjian = 'R'
+
+    when(nilaiUjian){
+        'A' -> println("Selamat Anda Mendapatkan Nilai = " +nilaiUjian)
+        'B' -> println("Selamat Anda Mendapatkan Nilai = " +nilaiUjian)
+        'C' -> println("Selamat Anda Mendapatkan Nilai = " +nilaiUjian)
+        'D' -> println("Selamat Anda Mendapatkan Nilai = " +nilaiUjian)
+        'E' -> println("Selamat Anda Mendapatkan Nilai = " +nilaiUjian)
+        else -> println("Maaf Anda Harus Mengulang")
+    }
+}
