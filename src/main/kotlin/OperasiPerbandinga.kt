afda0798bff9b52c49dd4f4edfdbfee4f9b3317c
arzailants <andrerizaldib@gmail.com>
2021-11-18 10:46:22 +0700

    Membuat Operasi Perbandingan

---
+++ src/main/kotlin/OperasiPerbandinga.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    // OPERASI PERBANDINGAN >
+    val nilaiInt = 80
+    val nilaiInt2 = 100
+    val logika = nilaiInt > nilaiInt2
+    println(logika)
+
+    // OPERASI PERBANDINGAN <
+    val data1 = 80
+    val data2 = 100
+    val logikaLessThan = data1 < data2
+    println(logikaLessThan)
+
+}
