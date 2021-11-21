3599c71fc3e51a5db8b6cfcad67abdd44188776e
arzailants <andrerizaldib@gmail.com>
2021-11-21 07:22:49 +0700

    Membuat For Loops Class

---
+++ src/main/kotlin/ForLoops.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+    // PERULANGAN FOR
+    val arrayValue = arrayOf("Andre Rizaldi Brillianto", "AliceMyLove", "RevethVeins", "BlackSpring", "BlackWinter")
+
+    for (arrayLooping in arrayValue){
+        println(arrayLooping)
+    }
+}
