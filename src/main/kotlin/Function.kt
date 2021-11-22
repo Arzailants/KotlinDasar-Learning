49090b1ac55d464d7a2dc1c5db7ae96bc444bcf3
arzailants <andrerizaldib@gmail.com>
2021-11-22 14:49:21 +0700

    Menambahkan Kelas Function

---
+++ src/main/kotlin/Function.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    // MEMANGGIL FUNCTION / METHOD
+    helloWorld()
+}
+
+// MEMBUAT FUNCTION / METHOD
+fun helloWorld(){
+    println("Ini adalah Method Hello World")
+    println("Selamat datang di Aplikasi CodeLamps")
+}
